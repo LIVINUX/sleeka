d4344eac64ef7bcc46337a56baeec2b77c57c07e
LIVINUX <34344374+LIVINUX@users.noreply.github.com>
2026-08-30 08:01:58 +0100

    Add files via upload

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -35,8 +35,7 @@ export const Hero: React.FC = () => {
 
         <p className="max-w-4xl mx-auto text-base sm:text-lg md:text-xl text-gray-200 font-light leading-relaxed mb-10 md:mb-16 px-2 sm:px-4">
           From strategy to execution, you get a dedicated team that helps you attract the right
-          customers and turn your content into a system for growth without the cost and stress
-          of building a full in-house marketing team.
+          customers and turn your content into a system for growth.
         </p>
 
         <div className="flex justify-center mb-8 md:mb-12 px-4">
